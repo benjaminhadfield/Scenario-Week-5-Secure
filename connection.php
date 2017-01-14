22f2822b8492dfe33026ed94df21c54fcc360172
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-01-14 00:12:41 +0000

    made db class

--- connection.php
+++ connection.php
@@ -2,36 +2,57 @@
 
 if (getenv('ENV') == 'PRODUCTION') {
 
-  // PHP Data Objects(PDO) Sample Code:
-  try {
-    $DB_PASSWORD = getenv('DB_PASSWORD');
-    $DB_USERNAME = getenv('DB_USERNAME');
+  class Db
+  {
+    private static $instance = NULL;
 
-//    $conn = new PDO("Database=comp205p_ae_secure;Data Source=eu-cdbr-azure-north-e.cloudapp.net;User Id=$DB_USERNAME;Password=$DB_PASSWORD");
-    $conn = new PDO("mysql:host=eu-cdbr-azure-north-e.cloudapp.net;dbname=comp205p_ae_secure;", $DB_USERNAME, $DB_PASSWORD);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch (PDOException $e) {
-    echo("<strong>Error connecting to MySQL.</strong><br>");
-    die(print_r($e));
-  }
+    private function __construct()
+    {
+    }
 
-  // SQL Server Extension Sample Code:
-  $connectionInfo = array("UID" => "comp205p-ae@comp205p-ae", "pwd" => "${$DB_SERVER_PASSWORD}", "Database" => "comp205p-ae-secure", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-  $serverName = "tcp:comp205p-ae.database.windows.net,1433";
-  $conn = sqlsrv_connect($serverName, $connectionInfo);
+    private function __clone()
+    {
+    }
+
+    public static function getInstance()
+    {
+      if(!isset(self::$instance)) {
+        try {
+          $DB_PASSWORD = getenv('DB_PASSWORD');
+          $DB_USERNAME = getenv('DB_USERNAME');
+
+          $conn = new PDO("mysql:host=eu-cdbr-azure-north-e.cloudapp.net;dbname=comp205p_ae_secure;", $DB_USERNAME, $DB_PASSWORD);
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          self::$instance = $conn;
+        } catch
+        (PDOException $e) {
+          echo("<strong>Error connecting to MySQL.</strong><br>");
+          die(print_r($e));
+        }
+      }
+      return self::$instance;
+    }
+  }
 } else {
 
   // Singleton pattern
-  class Db {
+  class Db
+  {
     private static $instance = NULL;
-    private function __construct() {}
-    private function __clone() {}
 
-    public static function getInstance() {
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function getInstance()
+    {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        self::$instance = new PDO('mysql:host=localhost;dbname=ucl_scenario_week_5_secure','ucl_sw_5_admin','FCQYxUFDWNrCtBYr', $pdo_options);
+        self::$instance = new PDO('mysql:host=localhost;dbname=ucl_scenario_week_5_secure', 'ucl_sw_5_admin', 'FCQYxUFDWNrCtBYr', $pdo_options);
       }
       return self::$instance;
     }
